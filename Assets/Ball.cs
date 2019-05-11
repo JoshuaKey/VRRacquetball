6dc0d8cfe852bdba1aa33ac00135ed524e6b6131
JoshuaKey <keyjoshj98@yahoo.com>
2019-05-10 18:10:59 -0600

    Added Ball

---
+++ Assets/Ball.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [Header("Components")]
+    public new Rigidbody rigidbody;
+    public new Collider collider;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+
+}
