ea5351a563a3ce3ba2d16ab17984a9fb48a2d4bf
MKetchum <MKetchum@student.neumont.edu>
2019-06-03 19:03:12 -0600

    Input Field
    Added an input field

---
+++ Assets/MobileInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileInput : InputField
+{
+    protected override void Start()
+    {
+        keyboardType = (TouchScreenKeyboardType)(-1);
+        base.Start();
+    }
+}
